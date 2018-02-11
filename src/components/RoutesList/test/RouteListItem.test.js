18136953fe2b381ab910edab6a12f2a22a4342d4
Colin <colinladley@hotmail.com>
2018-02-11 11:56:16 +0000

    Added RouteListItem test

---
+++ src/components/RoutesList/test/RouteListItem.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import RouteListItem from '../RouteListItem';
+import renderer from 'react-test-renderer';
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe('<RouteListItem />', () => {
+  const props = {
+    tag: 'C',
+    title: 'California',
+    color: 'fffff'
+  };
+
+  it('should render correctly', () => {
+    const wrapper = renderer.create(<RouteListItem {...props} />).toJSON();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('user clicks item', () => {
+    let wrapper;
+    let onClicked = jest.fn();
+
+    beforeEach(() => {
+      onClicked.mockClear();
+      wrapper = shallow(<RouteListItem {...props} onRouteClick={onClicked} />);
+      wrapper.simulate('click');
+    });
+
+    it('should call prop onRouteClick()', () => {
+      expect(onClicked.mock.calls.length).toBe(1);
+    });
+
+    it('should call prop onRouteClick() with correct tag', () => {
+      expect(onClicked.mock.calls[0][0]).toBe('C');
+    });
+  });
+});
