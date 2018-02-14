239323d2d4d60bfdf154c741be01ba9747579174
Colin <colinladley@hotmail.com>
2018-02-14 10:07:01 +0000

    Added redux-immutable-state-invariant to catch any errors in mutating state

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,9 +3,13 @@ import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import reducer from '../reducers/reducer';
 
+// Be sure to ONLY add this middleware in development!
+const middleware = process.env.NODE_ENV !== 'production' ?
+  [require('redux-immutable-state-invariant').default(), thunk] : [thunk];
+
 const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
